e4e71a7b1cabd32014ccb0cde663c4f330f9581d
Guilherme Sant Anna <guilherme.santanna.it@gmail.com>
2025-03-24 14:31:37 -0300

    24032025 - g

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 export default {
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.js$': 'babel-jest',
   },
   testEnvironment: 'node',
 };
--- src/controllers/aulaController.test.js
+++ src/controllers/aulaController.test.js
@@ -1,7 +1,6 @@
 import AulaController from './aulaController.js';
 import aula from '../models/Aula.js';
-import * as Usuario from '../models/Usuario.js';
-
+import {usuario} from '../models/Usuario.js';
 
 jest.mock('../models/Aula.js');
 jest.mock('../models/Usuario.js');
